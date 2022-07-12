303fb778488e2535b22b1bacaa706137971bdf87
Ankeyt <95373585+Ankeyt@users.noreply.github.com>
2022-07-12 19:10:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,11 @@ App.use(express.json())
 App.use('/',userRoute)
 
 
+//mongodbwrong://ankeet:<>@cluster0.rmpmo.mongodb.net/?retryWrites=true&w=majority
 
-
-//Mongoose.connect("mongodbwrong://ankeet:<>@cluster0.rmpmo.mongodb.net/?retryWrites=true&w=majority",()=>{
+Mongoose.connect("mongodb+srv://ankit:87654321@cluster0.rmpmo.mongodb.net/aadhar?retryWrites=true&w=majority",()=>{
     console.log('MongoDB connected')
-//})
+})
 
 App.listen(3002,()=>{
 console.log("Server running on port 3002")
